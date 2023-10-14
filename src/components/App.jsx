0b4993e2684114ca54c0a95ebb87016c0a8fba14
RayoKh <platypus1703@gmail.com>
2023-10-14 23:50:48 +0300

    done first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSubmit = values => {
     const { name, number } = values;
 
